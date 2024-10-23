1af4370483dbb2d23817004769eb020b5b7af16c
foudji <logan.lhote-baechler@jobtrek.ch>
2024-10-23 10:23:44 +0200

    feat!: removed some useless information who was displayed to the user. Changed the .txt to a JSON file and added a --delete flag

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,46 @@
-use std::io::{self, Write};
-use std::fs::OpenOptions;
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::{self, read_to_string}, io::{self, Read}, path, sync::TryLockResult
+};
 
-fn main() -> std::io::Result<()> {
-    let mut file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .append(true)
-        .open("todolist.txt")?;
+#[derive(Serialize, Deserialize)]
+struct Todo {
+    content: String,
+}
 
+fn main() {
     println!("Veuillez entrer une tâche que vous souhaitez accomplir :");
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
 
-    let todo = input.trim();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Impossible d'accéder à l'input de l'utilisateur");
 
-    if !todo.is_empty() {
-        println!("À effectuer : - {}", todo);
-        writeln!(file, "{}", todo)?;
-        println!("Tâche ajoutée à todolist.txt");
-    } else {
-        println!("Aucune tâche n'a été ajoutée.");
-    }
+    let mut todos: Vec<Todo> = match read_to_string("todolist.json") {
+        Ok(file_content) => {
+            serde_json::from_str(&file_content).expect("Impossible de deserialiser JSON")
+        }
+        Err(_) => Vec::new(),
+    };
 
-    Ok(())
+    let input = input.trim();
+    
+    if input.contains("--delete") {
+        let condition = input.split(" ").last().expect("Cannot find the task");
+        let todo_number: usize = condition.parse().expect("Cannot convert to number");
+        todos.remove(todo_number - 1);
+    };
+
+    todos.push(Todo {
+        content: input.to_string(),
+    });
+
+    let _ = fs::write(
+        "todolist.json",
+        serde_json::to_string(&todos).expect("Impossible de créer un fichier pour la todo"),
+    );
+
+
+
 }
